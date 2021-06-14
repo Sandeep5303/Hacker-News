086242e3213409b23726fa5fc588d1ae5bbc3328
sandeep5303 <paul.phoenix7285@gmail.com>
2021-06-14 20:42:51 +0530

    cleanup and readme

--- pages/api/[id].js
+++
@@ -1,6 +0,0 @@
-// const { news } = require('./data.json');
-
-// export default (req, res) => {
-//   const nw = news.filter(n => '' + n.id === req.query.id);
-//   res.status(200).json(nw);
-// };

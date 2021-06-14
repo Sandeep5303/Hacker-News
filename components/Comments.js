85d82f7c28f7888389d8685a740c911f68e1c796
sandeep5303 <paul.phoenix7285@gmail.com>
2021-06-14 13:10:30 +0530

    inner html cmts parse

--- components/Comments.js
+++ components/Comments.js
@@ -1,16 +1,19 @@
 import styles from '@styles/Comments.module.css';
-import { parseHTML } from '@config/helper';
 
 export default function Comments({ cmt }) {
+  const createMarkup = () => {
+    return { __html: `${cmt.text}` };
+  };
   return (
     <div className={styles.comments}>
       <div className={styles.cmtTop}>
         <small>{cmt.author}</small>
         <small>{cmt.created_at}</small>
       </div>
-      <div className={styles.cmtBottom}>
-        {cmt.text && <p>{parseHTML(cmt.text)}</p>}
-      </div>
+      <div
+        className={styles.cmtBottom}
+        dangerouslySetInnerHTML={createMarkup()}
+      ></div>
     </div>
   );
 }
